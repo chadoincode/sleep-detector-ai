d16431d5ceedc911ec8f357f5f40801ac4a8f13f
shaa/cha(?!) <ayeshacha177@gmail.com>
2025-07-02 21:02:30 +0700

    chore: initialize repository

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)  # biar frontend Astro bisa akses
+
+# Load model dari file
+with open("model/model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+@app.route("/predict", methods=["POST"])
+
+def predict():
+    data = request.get_json()
+    print("DATA MASUK:", data)
+
+    try:
+        # print("Tipe data masing-masing:")
+        # for key, val in data.items():
+        #     print(f"{key}: {val} ({type(val)})")
+        # Load model & scaler
+        with open("model/model.pkl", "rb") as f:
+            model = pickle.load(f)
+        with open("model/scaler.pkl", "rb") as f:
+            scaler = pickle.load(f)
+
+        features = [
+            int(data["age"]),
+            float(data["sleep_duration"]),
+            int(data["stress_level"]),
+            int(data["physical_activity_level"]),
+            int(data["heart_rate"]),
+            int(data["gender"])
+        ]
+
+        input_array = scaler.transform([features])
+        prediction = model.predict(input_array)
+
+        hasil = "Kualitas Tidur Buruk" if prediction[0] == 0 else "Kualitas Tidur Baik"
+        return jsonify({"hasil": hasil})
+
+    except Exception as e:
+        print("ERROR SAAT PREDIKSI:", e)
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
